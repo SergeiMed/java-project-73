956c1b3f996982c646a130e92748069adc94b9ce
SergeiMed <s.medvedev86@mail.ru>
2023-04-24 22:15:39 +0300

    fix User

--- app/src/main/java/hexlet/code/model/User.java
+++ app/src/main/java/hexlet/code/model/User.java
@@ -1,34 +1,54 @@
 package hexlet.code.model;
 
-
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.GenerationType;
 import javax.persistence.Temporal;
-import java.util.Date;
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
+import static javax.persistence.GenerationType.IDENTITY;
 import static javax.persistence.TemporalType.TIMESTAMP;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String firstName;
-    private String lastName;
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String email;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @JsonIgnore
+    private String password;
+
     @CreationTimestamp
     @Temporal(TIMESTAMP)
     private Date createdAt;
+
+    public User(final Long id) {
+        this.id = id;
+    }
 
 }
