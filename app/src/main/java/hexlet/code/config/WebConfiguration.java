92b8d09bcc9e933ea4933df14a368bb74d99c4ac
SergeiMed <s.medvedev86@mail.ru>
2023-05-01 16:36:09 +0300

    fix WebConfiguration

--- app/src/main/java/hexlet/code/config/WebConfiguration.java
+++ app/src/main/java/hexlet/code/config/WebConfiguration.java
@@ -1,13 +1,13 @@
 package hexlet.code.config;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
