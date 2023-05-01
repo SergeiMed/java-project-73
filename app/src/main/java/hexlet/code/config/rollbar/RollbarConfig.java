ce52f201c682ea7c7aad5a40a041d3fb4edd808c
SergeiMed <s.medvedev86@mail.ru>
2023-05-01 16:37:22 +0300

    add Rollbar

--- app/src/main/java/hexlet/code/config/rollbar/RollbarConfig.java
+++ app/src/main/java/hexlet/code/config/rollbar/RollbarConfig.java
@@ -3,41 +3,42 @@ package hexlet.code.config.rollbar;
 import com.rollbar.notifier.Rollbar;
 import com.rollbar.notifier.config.Config;
 import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
-@Configuration
+@Configuration()
+@EnableWebMvc
 @ComponentScan({
 
 // UPDATE TO YOUR PROJECT PACKAGE
-    "hexlet.code"
+    "hexlet.code",
+    "com.rollbar.spring"
 
 })
 public class RollbarConfig {
 
-    // Добавляем токен через переменные окружения
-    @Value("${rollbar_token:}")
-    private String rollbarToken;
-
-    @Value("${spring.profiles.active:}")
-    private String activeProfile;
-
     /**
      * Register a Rollbar bean to configure App with Rollbar.
      */
+    @Value("${rollbar_token:}")
+    private String rollbarToken;
+
     @Bean
     public Rollbar rollbar() {
+
         return new Rollbar(getRollbarConfigs(rollbarToken));
     }
 
     private Config getRollbarConfigs(String accessToken) {
 
+        // Reference ConfigBuilder.java for all the properties you can set for Rollbar
         return RollbarSpringConfigBuilder.withAccessToken(accessToken)
                 .environment("development")
-                .enabled(activeProfile == "prod")
+                .environment("production")
                 .build();
     }
 }
